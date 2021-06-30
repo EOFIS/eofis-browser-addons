57c6dcc68b85f459508665193153c2e97bc6941a
Tiarnach O Riada <tiarnachreidy@gmail.com>
2021-06-30 17:03:00 +0100

    Started web-extension addon

---
+++ web-extension/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+		<p>This is a hello from {{ defaultText }} with the message {{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+	props: {
+		msg: String
+		},
+  mounted() {
+    browser.runtime.sendMessage({});
+  },
+  computed: {
+    defaultText() {
+      return browser.i18n.getMessage("extName");
+    },
+  },
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+}
+</style>
---
+++ web-extension/src/override/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+	<h1>Override</h1>
+  <hello-world />
+	</div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: { HelloWorld },
+};
+</script>
