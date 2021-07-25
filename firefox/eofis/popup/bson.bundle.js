b945a1e2ef05cd74121871b15b251bfd0d7361ae
Tiarnach O Riada <tiarnachreidy@gmail.com>
2021-07-25 07:24:48 +0100

    Removed old firefox addon

--- firefox/eofis/popup/bson.bundle.js
+++
@@ -1,1 +0,0 @@
-/home/tor/projects/ucd/eeen30130_prof-eng-project/eofis/arch/addons/firefox/eofis/js-bson/dist/bson.bundle.js
--- firefox/eofis/scripts/install_linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mkdir ~/.mozilla/native-messaging-hosts -p
-# Have to get full path for source file hence readlink
-ln -s $(readlink -f ./app/eofis.json) ~/.mozilla/native-messaging-hosts/
